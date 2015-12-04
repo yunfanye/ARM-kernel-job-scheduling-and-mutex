81e8a95b76fa3e353154e059f6f42ab080d7e15a
fangdac <chenfangda@outlook.com>
2015-12-03 22:28:13 -0500

    22:30

--- kernel/include/lock.h
+++ kernel/include/lock.h
@@ -37,5 +37,6 @@ void mutex_init(void);	/* a function for initiating mutexes */
 int mutex_create(void);
 int mutex_lock(int mutex);
 int mutex_unlock(int mutex);
+int find_available_mutex();
 
 #endif /* _LOCK_H_ */
--- kernel/lock/mutex.c
+++ kernel/lock/mutex.c
@@ -25,22 +25,114 @@ mutex_t gtMutex[OS_NUM_MUTEX];
 
 void mutex_init()
 {
-	
+	int i = 0;
+	for(; i < OS_NUM_MUTEX; i++)
+	{
+		gtMutex[i].bAvailable = TRUE;
+		gtMutex[i].pHolding_Tcb = NULL;
+		gtMutex[i].bLock = FALSE;
+		gtMutex[i].pSleep_queue = NULL;
+
+	}
 }
 
 int mutex_create(void)
 {
-	
-	return 1; // fix this to return the correct value
+	//disable_interrupt;
+	int position = find_available_mutex();
+	if(position != -1)
+	{
+		gtMutex[position].bAvailable = FALSE;
+		return position;
+	}
+	else//do nothing
+	{
+		//enable_interrupts;
+		return -ENOMEM;
+	}
+
+	//enable_interrupts; // fix this to return the correct value
 }
 
 int mutex_lock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	//interrupt
+	tcb_t * current = get_cur_tcb();
+
+	if(mutex < 0 || mutex > OS_NUM_MUTEX)
+		return -EINVAL;
+
+	else if (gtMutex[mutex].bAvailable == TRUE)
+		return -EINVAL;
+
+	else if (gtMutex[mutex].bLock == TRUE && 
+		(gtMutex[mutex].pHolding_Tcb != NULL && gtMutex[mutex].pHolding_Tcb != current))
+	{
+		current -> sleep_queue = NULL;
+		if(gtMutex[mutex].pSleep_queue == NULL)
+		{
+			gtMutex[mutex].pSleep_queue = current;
+		}
+		else {
+			tcb_t * next = gtMutex[mutex].pSleep_queue;
+			
+			while(next -> sleep_queue != NULL)
+			{
+				next = next -> sleep_queue;
+			}
+			next -> sleep_queue = current;
+		}
+		dispatch_sleep();
+	}
+
+	gtMutex[mutex].bLock = TRUE;
+	gtMutex[mutex].pHolding_Tcb = current;
+	gtMutex[mutex].pSleep_queue = NULL;
+	current.holds_lock ++ ;
+	return 0;
 }
 
 int mutex_unlock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+
+	tcb_t * current = get_cur_tcb();
+
+	if(mutex < 0 || mutex > OS_NUM_MUTEX)
+		return -EINVAL;
+
+	else if (gtMutex[mutex].bAvailable == TRUE)
+		return -EINVAL;
+
+	else if (gtMutex[mutex].pHolding_Tcb != current)
+	{
+		return -EPERM;
+	}
+	else
+	{
+		gtMutex[mutex].pHolding_Tcb -> holds_lock --;
+		gtMutex[mutex].bLock = FALSE;
+		if(gtMutex[mutex].pSleep_queue != NULL)
+		{
+			gtMutex[mutex].pHolding_Tcb = gtMutex[mutex].pSleep_queue;
+			runqueue_add(gtMutex[mutex].pSleep_queue, gtMutex[mutex].pSleep_queue -> cur_prio);
+			gtMutex[mutex].pSleep_queue = gtMutex[mutex].pSleep_queue -> sleep_queue;
+		}
+		else
+		{
+    		gtMutex[mutex].pHolding_Tcb = NULL;
+		}
+		return 0;
+	}
+}
+
+int find_available_mutex()
+{
+	int i = 0;
+	for(; i < OS_NUM_MUTEX; i++)
+	{
+		if(gtMutex[i].bAvailable == TRUE)
+			return i;
+	}
+	return -1;
 }
 
