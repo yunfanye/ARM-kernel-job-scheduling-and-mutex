83d03ad99ce3bbb45fb5b151cb4661d3acd59771
Yunfan Ye <yunfanye@cmu.edu>
2015-12-04 13:52:30 -0500

    add boundary

---
+++ kernel/syscall/boundary.h
@@ -0,0 +1,9 @@
+#ifndef _BOUNDARY_H_
+#define _BOUNDARY_H_
+
+#define SDRAM_start					0xa0000000
+#define SDRAM_end					0xa3ffffff
+#define ROM_start					0x00000000
+#define ROM_end						0x00ffffff
+
+#endif
