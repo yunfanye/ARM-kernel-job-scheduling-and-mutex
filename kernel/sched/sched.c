df9d8e108d20291b909354ce97976c7e9ef9ab99
Yunfan Ye <yunfanye@cmu.edu>
2015-12-03 20:39:07 -0500

    c source code complete

--- kernel/sched/sched.c
+++ kernel/sched/sched.c
@@ -22,14 +22,31 @@ tcb_t system_tcb[OS_MAX_TASKS]; /*allocate memory for system TCBs */
 
 void sched_init(task_t* main_task  __attribute__((unused)))
 {
-	
+	sched_context_t* context;
+
+	system_tcb[IDLE_PRIO].native_prio = IDLE_PRIO;
+	system_tcb[IDLE_PRIO].cur_prio = IDLE_PRIO;
+	system_tcb[IDLE_PRIO].holds_lock = 0;
+	system_tcb[IDLE_PRIO].sleep_queue = NULL;
+
+	context = &(system_tcb[IDLE_PRIO].context);
+    context->r4 = (size_t)idle;
+    context->r5 = 0;
+    context->r6 = 0;
+    context->r7 = 0;
+    context->r8 = global_data;
+    context->r9 = 0;
+    context->r10 = 0;
+    context->r11 = 0;
+    context->sp = system_tcb[IDLE_PRIO].kstack_high;
+    context->lr = launch_task;
 }
 
 /**
  * @brief This is the idle task that the system runs when no other task is runnable
  */
  
-static void __attribute__((unused)) idle(void)
+static void idle(void)
 {
 	 enable_interrupts();
 	 while(1);
@@ -48,8 +65,29 @@ static void __attribute__((unused)) idle(void)
  * @param tasks  A list of scheduled task descriptors.
  * @param size   The number of tasks is the list.
  */
-void allocate_tasks(task_t** tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
+void allocate_tasks(task_t** tasks, size_t num_tasks)
 {
-	
+	int i;
+	sched_context_t* context;
+	for(i = 0; i < OS_MAX_TASKS; i++) {
+		system_tcb[i].native_prio = i;
+		system_tcb[i].cur_prio = i;
+		system_tcb[i].holds_lock = 0;
+		system_tcb[i].sleep_queue = NULL;
+
+		context = &(system_tcb[i].context);
+	    context->r4 = (size_t)task->lambda;
+	    context->r5 = (size_t)task->data;
+	    context->r6 = (size_t)task->stack_pos;
+	    context->r7 = 0;
+	    context->r8 = global_data;
+	    context->r9 = 0;
+	    context->r10 = 0;
+	    context->r11 = 0;
+	    context->sp = system_tcb[i].kstack_high;
+	    context->lr = launch_task;
+
+		runqueue_add(&system_tcb[i], i);
+	}
 }
 
--- kernel/syscall/proc.c
+++ kernel/syscall/proc.c
@@ -22,23 +22,67 @@
 #include <arm/physmem.h>
 #include <device.h>
 
-int task_create(task_t* tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
+
+/**
+ * @brief Sort all task using bubble sort
+ */
+void sort_tasks(task_t* tasks, size_t num_tasks)
 {
-	/* init devices */
+    size_t i;
+    size_t j;
+    task_t tmp;
+    for(i = 0; i < num_tasks; i++)
+    {
+        for(j = i + 1; j < num_tasks; j++)
+        {
+            if(tasks[i].T > tasks[j].T)
+            {
+            	/* SWAP two tasks */
+            	tmp = tasks[i];
+            	tasks[i] = tasks[j];
+            	tasks[j] = tmp;
+            }
+        }
+    }
+}
+
+int task_create(task_t* tasks, size_t num_tasks)
+{
+	if(num_tasks <= 0 || num_tasks > OS_AVAIL_TASKS)
+        return -EINVAL;
+	/* init devices and run queue */
 	dev_init();
-	
-	
-	
+	runqueue_init();
+	/* sort tasks, rate monotonic */
+	sort_tasks(tasks, num_tasks);
+	/* init scheduler */
+	sched_init();
+	allocate_tasks(tasks, num_tasks);
+
+	/* switch from idle to task */
+	dispatch_nosave();
+
 	return 1; /* remove this line after adding your code */
 }
 
-int event_wait(unsigned int dev  __attribute__((unused)))
+int event_wait(unsigned int dev)
 {
-  return 1; /* remove this line after adding your code */	
+    tcb_t* cur_tcb = get_cur_tcb();
+    if(dev >= NUM_DEVICES)
+        return -EINVAL;
+
+    if(cur_tcb->holds_lock >= 1)
+        return -EHOLDSLOCK;
+
+    // Puts the calling task to sleep on given device number
+    dev_wait(dev);
+    dispatch_sleep();
+    
+    return 0;
 }
 
 /* An invalid syscall causes the kernel to exit. */
-void invalid_syscall(unsigned int call_num  __attribute__((unused)))
+void invalid_syscall(unsigned int call_num)
 {
 	printf("Kernel panic: invalid syscall -- 0x%08x\n", call_num);
 
