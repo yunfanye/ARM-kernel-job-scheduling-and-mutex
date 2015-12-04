6302b018b56d62549fd91e1e58c3fbeb4c93f2df
Yunfan Ye <yunfanye@cmu.edu>
2015-12-04 14:33:31 -0500

    add addr

---
+++ kernel/include/arm/addr.h
@@ -0,0 +1,14 @@
+/**
+ * @file addr.h
+ *
+ * @brief Definitions address constants
+ *
+ * @author Yunfan Ye <yunfanye@cmu.edu>
+ *				 Fangda Chen <fangdac@andrew.cmu.edu>
+ *
+ * @date 2015-10-29
+ */
+/* stack pointer for svc state */
+#define SP_SAVED_LOC			0xa2000000
+/* stack pointer for IRQ state */
+#define IRQ_SP_LOC				0xa2000000
